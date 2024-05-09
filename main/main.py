2f66a32c00f3cce56820a985fd6d60e420fd6016
ga111o <ga111o@proton.me>
2024-05-09 22:39:41 +0900

    💩 i hate python

---
+++ main/main.py
@@ -0,0 +1,9 @@
+from langchain.chat_models import ChatOllama
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OllamaEmbeddings, CacheBackedEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.storage import LocalFileStore
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
