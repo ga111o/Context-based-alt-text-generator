a4694cfeac9ab454dcfc4272dbe800ca648d25a4
ga111o <ga111o@proton.me>
2024-05-10 19:04:45 +0900

    🏗 basic logic done

--- main/main.py
+++ main/main.py
@@ -1,9 +1,130 @@
+from langchain.agents import initialize_agent
 from langchain.chat_models import ChatOllama
-from langchain.callbacks.base import BaseCallbackHandler
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OllamaEmbeddings, CacheBackedEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.storage import LocalFileStore
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+from langchain.callbacks import StreamingStdOutCallbackHandler
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+
+from tools import ImageCaptionTool, ObjectDetectionTool
+
+from transformers import BlipProcessor, BlipForConditionalGeneration, DetrImageProcessor, DetrForObjectDetection
+from PIL import Image
+import torch
+import os
+import json
+
+def get_image_caption(image_path):
+    """
+    Generates a short caption for the provided image.
+
+    Args:
+        image_path (str): The path to the image file.
+
+    Returns:
+        str: A string representing the caption for the image.
+    """
+    image = Image.open(image_path).convert('RGB')
+
+    model_name = "Salesforce/blip-image-captioning-large"
+    device = "cpu"  # cuda
+
+    processor = BlipProcessor.from_pretrained(model_name)
+    model = BlipForConditionalGeneration.from_pretrained(model_name).to(device)
+
+    inputs = processor(image, return_tensors='pt').to(device)
+    output = model.generate(**inputs, max_new_tokens=20)
+
+    caption = processor.decode(output[0], skip_special_tokens=True)
+
+    return caption
+
+def detect_objects(image_path):
+    """
+    Detects objects in the provided image.
+
+    Args:
+        image_path (str): The path to the image file.
+
+    Returns:
+        str: A string with all the detected objects. Each object as '[x1, x2, y1, y2, class_name, confindence_score]'.
+    """
+    image = Image.open(image_path).convert('RGB')
+
+    processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
+    model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50")
+
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+
+    # convert outputs (bounding boxes and class logits) to COCO API
+    # let's only keep detections with score > 0.9
+    target_sizes = torch.tensor([image.size[::-1]])
+    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+
+    detections = ""
+    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+        detections += '[{}, {}, {}, {}]'.format(int(box[0]), int(box[1]), int(box[2]), int(box[3]))
+        detections += ' {}'.format(model.config.id2label[int(label)])
+        detections += ' {}\n'.format(float(score))
+
+    return detections
+
+
+tools = [ImageCaptionTool(), ObjectDetectionTool()]
+
+conversational_memory = ConversationBufferWindowMemory(
+    memory_key='chat_history',
+    k=5,
+    return_messages=True
+)
+
+llm = ChatOllama(
+    model = "llama3:8b",
+    temperature=0.1,
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()]
+)
+
+agent = initialize_agent(
+    agent="chat-conversational-react-description",
+    tools=tools,
+    llm=llm,
+    max_iterations=5,
+    verbose=True,
+    memory=conversational_memory,
+    early_stopping_method='generate'
+)
+
+context = "very happy situation"
+language = "korean"
+# user_question = f"Describe the visual elements of the image in one line based {context}. and translate to {language}"
+user_question = f"Describe the visual elements of the image in one line based {context}."
+
+if not os.path.exists('responses/'):
+    os.makedirs('responses/')
+
+image_files = os.listdir('imgs/')
+
+for image_name in image_files:
+    tools = [ImageCaptionTool(), ObjectDetectionTool()]
+
+    image_path = 'imgs/' + image_name
+    response = agent.run('{}, this is the image path: {}'.format(user_question, image_path))
+    print(image_path)
+    print(response)
+    
+    response_file_path = os.path.join('responses/', "output.json")
+    
+    if os.path.exists(response_file_path):
+        with open(response_file_path, 'r', encoding='utf-8') as file:
+            try:
+                data = json.load(file)
+            except json.JSONDecodeError:
+                data = {}
+    else:
+        data = {}
+    
+    data[image_name] = {"image_name": image_name, "response": response}
+    
+    with open(response_file_path, 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+# 이름 바꾸고 /main 디렉토리로 옮기기
