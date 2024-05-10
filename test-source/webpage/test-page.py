49f516f99c80d8c61d0e6d3b3efe06fd12817194
ga111o <ga111o@proton.me>
2024-05-10 22:00:53 +0900

    🚧 add /output route

--- test-source/webpage/test-page.py
+++ test-source/webpage/test-page.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def show_image():
     return render_template("./index.html")
+
+@app.route('/output')
+def output_json():
+    with open('./output.json', 'r', encoding='utf-8') as file:
+        data = file.read()
+
+    from flask import Response
+    return Response(data, mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8123)
