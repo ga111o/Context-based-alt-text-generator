f243a77b375efa114a822200c930e5c32514d6f8
ga111o <ga111o@proton.me>
2024-05-12 01:37:00 +0900

    🎨 format code

--- test-source/webpage/test-page.py
+++ test-source/webpage/test-page.py
@@ -6,13 +6,5 @@ app = Flask(__name__)
 def show_image():
     return render_template("./index.html")
 
-@app.route('/output')
-def output_json():
-    with open('./output.json', 'r', encoding='utf-8') as file:
-        data = file.read()
-
-    from flask import Response
-    return Response(data, mimetype='application/json')
-
 if __name__ == '__main__':
     app.run(debug=True, port=8123)
