de84f4f090fc8e00ab1df572a217688fc0522e50
ga111o <ga111o@proton.me>
2024-05-11 11:08:33 +0900

    🏗 add exception handling

--- main/server.py
+++ main/server.py
@@ -12,16 +12,23 @@ app = Flask(__name__)
 def working():
     return "working..."
 
-#http://127.0.0.1:9990/url?url=testing
+from requests.exceptions import ConnectionError
+
 @app.route('/url')
-def store_string():
+def get_url_n_img():
     url = request.args.get('url', default='', type=str)
     output_folder = "./imgs"
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
+        if response.status_code != 200:
+            return "connection error 1"
+    except ConnectionError:
+        return "connection error 2"
+
     soup = BeautifulSoup(response.content, "html.parser")
 
     img_tags = soup.find_all("img")
@@ -30,18 +37,22 @@ def store_string():
         if not img.has_attr('alt'):
             img_url = img.get("src")
             if img_url:
-                full_img_url = urljoin(url, img_url)  
-                img_response = requests.get(full_img_url, stream=True)
-                img_content_type = img_response.headers['Content-Type']
-                img_extension = guess_extension(img_content_type)
-                img_name = f"{i}{img_extension}"
-                img_path = os.path.join(output_folder, img_name)
-                with open(img_path, "wb") as img_file:
-                    for chunk in img_response.iter_content(chunk_size=8192):
-                        img_file.write(chunk)
-                print(f"donwload: {img_path}")
+                try:
+                    full_img_url = urljoin(url, img_url)  
+                    img_response = requests.get(full_img_url, stream=True)
+                    if img_response.status_code == 200:
+                        img_content_type = img_response.headers['Content-Type']
+                        img_extension = guess_extension(img_content_type)
+                        img_name = f"{i}{img_extension}"
+                        img_path = os.path.join(output_folder, img_name)
+                        with open(img_path, "wb") as img_file:
+                            for chunk in img_response.iter_content(chunk_size=8192):
+                                img_file.write(chunk)
+                        print(f"download: {img_path}")
+                except ConnectionError:
+                    print(f"Failed to download image at {img_url}")
 
-    return f"Stored string: {url}, "
+    return f"url: {url}, download done"
 
 @app.route('/output')
 def output_json():
