633a73c485071de0598adf36954653e6f5545738
ga111o <ga111o@proton.me>
2024-05-12 01:31:19 +0900

    🐛 solve CORS & img name problem

--- main/server.py
+++ main/server.py
@@ -1,26 +1,29 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, make_response
 import os
 import requests
 from bs4 import BeautifulSoup
 from mimetypes import guess_extension
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 import subprocess
 import time
 from requests.exceptions import ConnectionError
 
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def working():
-    return "working..."
+	return "working..."
 
 def wait_for_file(file_path, timeout=60):
-    start_time = time.time()
-    while not os.path.exists(file_path):
-        time.sleep(1)
-        if time.time() - start_time > timeout:
-            return False
-    return True
+	start_time = time.time()
+	while not os.path.exists(file_path):
+		time.sleep(1)
+		if time.time() - start_time > timeout:
+			return False
+	return True
 
 # todo
 # 함수 너무 길어지는 거 나눠야할듯
@@ -42,17 +45,28 @@ def get_url_n_img():
     soup = BeautifulSoup(response.content, "html.parser")
     img_tags = soup.find_all("img")
 
+    downloaded_files = set()  
+
     for i, img in enumerate(img_tags):
         if not img.has_attr('alt'):
             img_url = img.get("src")
             if img_url:
+                full_img_url = urljoin(url, img_url)
+                img_name = os.path.basename(urlparse(full_img_url).path) 
+
+                # # 파일명 중복 방지를 위한 처리
+                # original_img_name = img_name
+                # count = 1
+                # while img_name in downloaded_files:
+                #     name, ext = os.path.splitext(original_img_name)
+                #     img_name = f"{name}_{count}{ext}"
+                #     count += 1
+
+                downloaded_files.add(img_name)
+
                 try:
-                    full_img_url = urljoin(url, img_url)  
                     img_response = requests.get(full_img_url, stream=True)
                     if img_response.status_code == 200:
-                        img_content_type = img_response.headers['Content-Type']
-                        img_extension = guess_extension(img_content_type)
-                        img_name = f"{i}{img_extension}"
                         img_path = os.path.join(output_folder, img_name)
                         with open(img_path, "wb") as img_file:
                             for chunk in img_response.iter_content(chunk_size=8192):
@@ -70,9 +84,12 @@ def get_url_n_img():
 
 @app.route('/output')
 def output_json():
-    with open('./responses/output.json', 'r', encoding='utf-8') as file:
-        data = file.read()
-    return Response(data, mimetype='application/json')
-
+    try:
+        with open('./responses/output.json', 'r', encoding='utf-8') as file:
+            data = file.read()
+        return Response(data, mimetype='application/json')
+    except FileNotFoundError:
+        return make_response('', 404)
+    
 if __name__ == '__main__':
     app.run(debug=True, port=9990)
