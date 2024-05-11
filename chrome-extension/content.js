f2d17edcc68c3afb43ca566e1c336736d460c9e5
ga111o <ga111o@proton.me>
2024-05-12 08:21:58 +0900

    🔇 remove logs

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -5,16 +5,16 @@ const targetUrl = `http://127.0.0.1:9990/url?url=${encodeURIComponent(
 
 fetch(targetUrl)
   .then((response) => {
-    console.log("success - fetch(targetUrl):", response);
+    // console.log("success - fetch(targetUrl):", response);
   })
   .catch((error) => {
-    console.error("error - fetch(targetUrl):", error);
+    // console.error("error - fetch(targetUrl):", error);
   });
 
 async function updateImageAltText() {
   const response = await fetch("http://127.0.0.1:9990/output");
   if (!response.ok) {
-    console.error("failed to fetch alt texts");
+    // console.error("failed to fetch alt texts");
     return;
   }
 
@@ -22,20 +22,14 @@ async function updateImageAltText() {
 
   document.querySelectorAll("img").forEach((img) => {
     const imageName = img.src.split("/").pop();
-    console.log(`img name: ${imageName}`);
+    // console.log(`img name: ${imageName}`);
     if (altTexts[imageName]) {
       img.alt = altTexts[imageName].response;
     }
   });
 }
 
-// if (document.readyState === "loading") {
-//   document.addEventListener("DOMContentLoaded", updateImageAltText());
-// } else {
-//   updateImageAltText();
-// }
-// 이거 왜 작동을 안하는 거지
-
+// 음.... 이렇게 하는 방법밖에 없는 건가
 setInterval(function () {
   console.log("working...");
   if (document.readyState === "loading") {
