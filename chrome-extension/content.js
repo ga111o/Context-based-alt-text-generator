0c6d8593535730cf3225f9399b6d3a48dd5feb9d
ga111o <ga111o@proton.me>
2024-05-11 10:55:22 +0900

    🏗 add req url

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,24 +1,40 @@
-async function updateImageAltText() {
-  const response = await fetch("http://127.0.0.1:8123/output");
-  if (!response.ok) {
-    console.error("Failed to fetch alt texts");
-    return;
-  }
+// async function updateImageAltText() {
+//   const response = await fetch("http://127.0.0.1:8123/output");
+//   if (!response.ok) {
+//     console.error("Failed to fetch alt texts");
+//     return;
+//   }
 
-  const altTexts = await response.json();
+//   const altTexts = await response.json();
 
-  document.querySelectorAll("img").forEach((img) => {
-    const imageName = img.src.split("/").pop();
-    if (altTexts[imageName]) {
-      img.alt = altTexts[imageName].response;
-    } else if (!img.alt) {
-      img.alt = "no exist alt";
-    }
+//   document.querySelectorAll("img").forEach((img) => {
+//     const imageName = img.src.split("/").pop();
+//     if (altTexts[imageName]) {
+//       img.alt = altTexts[imageName].response;
+//     } else if (!img.alt) {
+//       img.alt = "no exist alt";
+//     }
+//   });
+// }
+
+// if (document.readyState === "loading") {
+//   document.addEventListener("DOMContentLoaded", updateImageAltText);
+// } else {
+//   updateImageAltText();
+// }
+
+const currentUrl = window.location.href;
+
+const targetUrl = `http://127.0.0.1:9990/url?url=${encodeURIComponent(
+  currentUrl
+)}`;
+
+fetch(targetUrl, {
+  method: "GET",
+})
+  .then((response) => {
+    console.log("Success:", response);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
   });
-}
-
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", updateImageAltText);
-} else {
-  updateImageAltText();
-}
