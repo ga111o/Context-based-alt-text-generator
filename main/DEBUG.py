a886385a7090e6162fa72cd623418b9a570af91b
ga111o <ga111o@proton.me>
2024-05-30 21:29:44 +0900

    📝 auto detect cuda or cpu

--- main/DEBUG.py
+++ main/DEBUG.py
@@ -3,8 +3,9 @@ CREATE_FILE_FOR_CHECK_LINE_BOOLEN = False # true or false
 
 # cpu: 1
 # cuda: 2 
-SELECT_DEVICE = 1
+# auto detected: 3
+SELECT_DEVICE = 3
 
 # 1: llama8:3b
 # 2: gpt3.5-turbo
-SELECT_LLM = 2
+SELECT_LLM = 1
--- main/tools.py
+++ main/tools.py
@@ -22,6 +22,8 @@ class ImageCaptionTool(BaseTool):
             device = "cpu"
         elif DEBUG.SELECT_DEVICE == 2:
             device = "cuda"
+        elif DEBUG.SELECT_DEVICE == 3:
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         processor = BlipProcessor.from_pretrained(model_name)
         model = BlipForConditionalGeneration.from_pretrained(model_name).to(device)
