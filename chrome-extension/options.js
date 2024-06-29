2e152e9dcdb2247f363000755c3305cd57b47b18
ga111o <ga111o@proton.me>
2024-06-29 12:44:37 +0900

    ✨ api key option

--- chrome-extension/options.js
+++ chrome-extension/options.js
@@ -11,13 +11,36 @@ document.addEventListener("DOMContentLoaded", () => {
   );
 });
 
-document.getElementById("save").addEventListener("click", function () {
+document.getElementById("save").addEventListener("click", async function () {
   const language = document.getElementById("language").value;
   const server = document.getElementById("server").value;
   const debugMode = document.getElementById("debugMode").checked;
   const apiKey = document.getElementById("apiKey").value;
+  const apiKeyErrorElement = document.getElementById("apiKeyError");
+
+  const isValidApiKey = await validateApiKey(apiKey);
+  if (!isValidApiKey) {
+    apiKeyErrorElement.style.display = "block";
+    return;
+  } else {
+    apiKeyErrorElement.style.display = "none";
+  }
 
   chrome.storage.sync.set({ language, server, debugMode, apiKey }, function () {
-    console.log("Language, Server, Debug Mode and API Key settings saved");
+    console.log("saved language, Server, Debug Mode and API Key settings");
   });
 });
+
+async function validateApiKey(apiKey) {
+  try {
+    const response = await fetch("https://api.openai.com/v1/engines", {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+    return response.ok;
+  } catch (error) {
+    console.error("api Key err:", error);
+    return false;
+  }
+}
