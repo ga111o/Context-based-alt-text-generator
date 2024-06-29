6de341dce9ae961724549b6fb4f96f1e99a39ea8
ga111o <ga111o@proton.me>
2024-06-29 12:38:53 +0900

    ✨ api key option

--- chrome-extension/options.js
+++ chrome-extension/options.js
@@ -1,19 +1,23 @@
-// options.js
-
 document.addEventListener("DOMContentLoaded", () => {
-  chrome.storage.sync.get(["language", "debugMode", "server"], (data) => {
-    document.getElementById("language").value = data.language || "English";
-    document.getElementById("server").value =
-      data.server || "http://127.0.0.1:9990";
-    document.getElementById("debugMode").checked = data.debugMode || false;
-  });
+  chrome.storage.sync.get(
+    ["language", "debugMode", "server", "apiKey"],
+    (data) => {
+      document.getElementById("language").value = data.language || "English";
+      document.getElementById("server").value =
+        data.server || "http://127.0.0.1:9990";
+      document.getElementById("debugMode").checked = data.debugMode || false;
+      document.getElementById("apiKey").value = data.apiKey || "";
+    }
+  );
 });
 
 document.getElementById("save").addEventListener("click", function () {
   const language = document.getElementById("language").value;
   const server = document.getElementById("server").value;
   const debugMode = document.getElementById("debugMode").checked;
-  chrome.storage.sync.set({ language, server, debugMode }, function () {
-    console.log("Language, Server and Debug Mode settings saved");
+  const apiKey = document.getElementById("apiKey").value;
+
+  chrome.storage.sync.set({ language, server, debugMode, apiKey }, function () {
+    console.log("Language, Server, Debug Mode and API Key settings saved");
   });
 });
