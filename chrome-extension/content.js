06651f33d063ee3be9ebd3109b5341d94daa7d93
ga111o <ga111o@proton.me>
2024-06-29 15:28:23 +0900

    ✨ api key option

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -6,23 +6,44 @@ let session = new Date().getTime();
 // let server = "http://127.0.0.1:9990";
 // let server = "http://10.5.12.10:9990";
 let server = "";
-chrome.storage.sync.get(["language", "debugMode", "server"], function (data) {
-  let language = data.language || "English";
-  debugMode = !!data.debugMode;
-  server = data.server || "http://127.0.0.1:9990";
+chrome.storage.sync.get(
+  ["language", "debugMode", "server", "apiKey"],
+  function (data) {
+    let language = data.language || "English";
+    debugMode = !!data.debugMode;
+    server = data.server || "http://127.0.0.1:9990";
+    let apiKey = data.apiKey || "";
 
-  const targetUrl = `${server}/url?url=${encodeURIComponent(
-    currentUrl
-  )}&language=${language}&session=${session}&title=${document.title}`;
+    const targetUrl = `${server}/url?url=${encodeURIComponent(
+      currentUrl
+    )}&language=${language}&session=${session}&title=${document.title}`;
 
-  fetch(targetUrl)
-    .then((response) => {
-      if (debugMode) console.log("success - fetch(targetUrl):", response);
-    })
-    .catch((error) => {
-      if (debugMode) console.error("error - fetch(targetUrl):", error);
-    });
-});
+    if (apiKey) {
+      fetch(`${server}/apikey`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ apiKey: apiKey, session: session }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (debugMode) console.log("success - fetch api key: ", data);
+        })
+        .catch((error) => {
+          if (debugMode) console.error("error - fetch api key:", error);
+        });
+
+      fetch(targetUrl)
+        .then((response) => {
+          if (debugMode) console.log("success - fetch(targetUrl):", response);
+        })
+        .catch((error) => {
+          if (debugMode) console.error("error - fetch(targetUrl):", error);
+        });
+    }
+  }
+);
 
 async function updateImageAltText() {
   const response = await fetch(`${server}/${session}/output`);
--- chrome-extension/options.js
+++ chrome-extension/options.js
@@ -1,3 +1,5 @@
+// options.js
+
 document.addEventListener("DOMContentLoaded", () => {
   chrome.storage.sync.get(
     ["language", "debugMode", "server", "apiKey"],
@@ -11,36 +13,12 @@ document.addEventListener("DOMContentLoaded", () => {
   );
 });
 
-document.getElementById("save").addEventListener("click", async function () {
+document.getElementById("save").addEventListener("click", function () {
   const language = document.getElementById("language").value;
   const server = document.getElementById("server").value;
   const debugMode = document.getElementById("debugMode").checked;
   const apiKey = document.getElementById("apiKey").value;
-  const apiKeyErrorElement = document.getElementById("apiKeyError");
-
-  const isValidApiKey = await validateApiKey(apiKey);
-  if (!isValidApiKey) {
-    apiKeyErrorElement.style.display = "block";
-    return;
-  } else {
-    apiKeyErrorElement.style.display = "none";
-  }
-
   chrome.storage.sync.set({ language, server, debugMode, apiKey }, function () {
-    console.log("saved language, Server, Debug Mode and API Key settings");
+    console.log("Language, Server, Debug Mode and API Key settings saved");
   });
 });
-
-async function validateApiKey(apiKey) {
-  try {
-    const response = await fetch("https://api.openai.com/v1/engines", {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-    });
-    return response.ok;
-  } catch (error) {
-    console.error("api Key err:", error);
-    return false;
-  }
-}
